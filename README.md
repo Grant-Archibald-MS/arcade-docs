19bdd05af939a3dcd1c7460f0dd9cbbe8987fc98
Grant Archibald <grant.archibald@microsoft.com>
2021-04-18 16:31:31 -0700

    Adding sample game

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Overview
 
-These documents explain how to leverage the power of Microsoft Make Code to build and create games that can be privately hosted in GitHub and hosted in different locations.
+These documents explain how to leverage the power of Microsoft Make Code Arcade to build and create games that can be privately hosted in GitHub and hosted in different locations.
 
-The [Wiki](../../wiki/) provides guides on how to get started and manage the process of updating your game.
+The [Guide](https://github.com/Grant-Archibald-MS/arcade-docs/wiki) provides instructions on how to get started and manage the process of updating your game.
---
+++ test1/tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100002020202010303030303030303030303070707070603030303030303030303030707070706030303030303090303080307070707060303030803030303030303050505050403030303030303030303030303030303030303030303030303030303030303030303030303030308030303030903090303030303030303030303030303030303030303030303030303030303030303030308030303030303090303030303080303030303030303030303030303030303030303030303030303030303030303030303030303030803030303030303030303030303030903030303030309030303030803030303030303030303030303030303030303030303030303`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath3,sprites.castle.tilePath2,sprites.castle.tileGrass1,sprites.castle.tilePath9,sprites.castle.tilePath8,sprites.castle.tilePath6,sprites.castle.tilePath5,sprites.builtin.forestTiles0,sprites.castle.tileGrass2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
