263625bae598966bf9d969b75028fce0e722d441
Grant Archibald <grant.archibald@microsoft.com>
2021-04-19 14:14:04 -0700

    Adding shell script to create site

---
+++ game-deploy.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+resourceGroup=$1
+location=$2
+
+echo "Create resource group $resourceGroup in $location"
+az group create --name $resourceGroup --location $location > /dev/null
+
+echo "Create FreePlan Application Plan"
+$plan = (az appservice plan create --resource-group $resourceGroup --name FreePlan --sku FREE > /dev/null
+uuid=$(uuidgen)
+uuid=$(echo ${uuid//-/})
+siteName = "game-" + $uuid
+
+echo "Create Web App"
+az webapp create --name $siteName --plan FreePlan --resource-group $resourceGroup  --runtime "php|7.2" > /dev/null
+
+echo "Copy Publish Profile to Github secret --> AZUREWEBAPPPUBLISHPROFILE"
+az webapp deployment list-publishing-profiles -g $resourceGroup -n $siteName --xml --output tsv
