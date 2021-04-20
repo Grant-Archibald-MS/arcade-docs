9542bd3fd1545655b5f1a18477a628bc7e8bdf40
Grant Archibald <grant.archibald@microsoft.com>
2021-04-19 17:49:58 -0700

    Script update

--- game-deploy.sh
+++ game-deploy.sh
@@ -8,7 +8,7 @@ echo "Create resource group $resourceGroup in $location"
 az group create --name $resourceGroup --location $location > /dev/null
 
 echo "Create FreePlan Application Plan"
-$plan = (az appservice plan create --resource-group $resourceGroup --name FreePlan --sku FREE > /dev/null
+az appservice plan create --resource-group $resourceGroup --name FreePlan --sku FREE > /dev/null
 uuid=$(uuidgen)
 uuid=$(echo ${uuid//-/})
 siteName = "game-" + $uuid
